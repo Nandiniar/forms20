804e22c39a61c91617696f1595a92b528bcb45d0
nandini <nandani@contaque.com>
2025-05-12 15:50:04 +0530

    Add these changes
    Signed-off-by: nandini <nandani@contaque.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'forms20';
+   // angular me 2 types of forms hote hai
+   // 1 reactive - usable,scalable,testable  yeh tab use hota hai jab  bahut jayda data like  bahut sare inpur field , humara 
+   //  data complex hota hai wo jo mushkil hota hia usse karna ho , isme hum functions ka use karte hai
+   //  2 template driven jab simple form se deal karna ho, isme hum directives ka use karte hai
 }
